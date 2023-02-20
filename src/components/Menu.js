cdf0315b59cfcd1052d2a55bdf823546976e9b59
Jeremy La <jeremylai511@gmail.com>
2023-02-20 00:59:10 -0500

    Added Menu functionality

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,16 +3,16 @@ import MenuItem from './MenuItem'
 
 const Menu = () => {
     const items = [
-        { component: 'Weather', label: 'Check the weather', icon: 'sun' },
-        { component: 'Todo', label: 'See your todo list', icon: 'todo' },
-        { component: 'Calendar', label: 'Open your calendar', icon: 'calendar' },
-        { component: 'Quote', label: 'Get a new quote', icon: 'quote' },
-        { component: 'Music', label: 'Listen to some music', icon: 'music' }
+        { component: 'Weather', label: 'Check the weather', icon: 'sun', height: 'max-h-60' },
+        { component: 'Todo', label: 'See your todo list', icon: 'todo', height: 'max-h-52'},
+        { component: 'Calendar', label: 'Open your calendar', icon: 'calendar', height: 'max-h-40' },
+        { component: 'Quote', label: 'Get a new quote', icon: 'quote', height: 'max-h-32' },
+        { component: 'Music', label: 'Listen to some music', icon: 'music', height: 'max-h-20' }
     ]
 
     return (
         <div className='flex flex-col mt-2 justify-center items-center w-1/5'>
-            <span className='m-2'>What would you like to do?</span>
+            <span className='m-2 select-none text-center w-full text-xl'>What would you like to do?</span>
             {items.map(item => (<MenuItem key={item.component} item={item}/>))}
         </div>
     )
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import { FaChevronRight } from 'react-icons/fa'
+import { useState } from 'react'
+import { FaChevronDown } from 'react-icons/fa'
 
 const MenuItem = ({ item }) => {
+    const [open, setOpen] = useState(false);
+
+    const toggleOpen = () => {
+        setOpen(!open);
+    }
+
     return (
-        <div className='w-full border border-transparent rounded-md flex justify-center align-center m-2 p-2 bg-white bg-opacity-10 hover:cursor-pointer hover:bg-opacity-60'>
-            <div className='mr-auto invisible'><FaChevronRight /></div>
-            <div className=''>{item.label}</div>
-            <div className='ml-auto my-auto'><FaChevronRight /></div>
+        <div className="border border-transparent rounded-md flex-col justify-center align-center bg-white bg-opacity-10 mt-2">
+            <div onClick={toggleOpen} className="px-3 w-96 border border-transparent rounded-md items-center h-12 select-none flex justify-center align-center bg-white bg-opacity-10 hover:cursor-pointer hover:bg-opacity-60 transition">
+                <div className='mr-auto invisible'><FaChevronDown /></div>
+                <div className=''>{item.label}</div>
+                <div className='ml-auto my-auto'><FaChevronDown /></div>
+            </div>
+            <div className={`px-6 pt-0 overflow-hidden transition-[max-height] duration-700 ease-in-out ${open ? item.height : "max-h-0"}`}>
+                <p className="pb-4 py-2 text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus ornare leo nec ultricies. Pellentesque purus velit, iaculis sit amet volutpat at, imperdiet sit amet metus. Etiam accumsan efficitur nibh, eget ornare urna pulvinar id. Cras tempus, elit in sodales vehicula, neque ipsum lobortis eros, nec pulvinar eros magna quis magna. In eleifend pretium sapien, id ornare nisi sollicitudin nec. Etiam diam ante, tempor semper egestas sed, mattis quis sem. Morbi sollicitudin, orci non cursus ornare, tellus nulla ultricies metus, eu tincidunt ligula dui quis dui. Pellentesque sed mollis nisi, quis eleifend odio. Nulla id elit cursus turpis ornare ultricies vel in nibh. Aliquam dignissim aliquet congue. Donec tristique lectus mauris, eu ullamcorper ipsum pharetra et. Duis ultricies arcu ut ex pellentesque, non luctus dolor malesuada. Ut congue sollicitudin tortor, efficitur feugiat dui porta in. Etiam a eros vulputate purus tempus venenatis. Integer in nunc quis lorem tempor consectetur.
+                </p>
+            </div>
         </div>
     )
 }
