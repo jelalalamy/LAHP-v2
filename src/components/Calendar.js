2ebcb496d8dcd99ed7ea4b0d27424b321fa94c04
Jeremy La <jeremylai511@gmail.com>
2023-02-25 03:29:16 -0500

    Added new Menu items

---
+++ src/components/Calendar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <div className="flex justify-center">
+      <div className='w-96 h-80 flex flex-col border-none py-4'>
+        <span className="text-9xl m-auto select-none">
+          yay
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Calendar
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import MenuItem from './MenuItem'
-import Clock from './Clock'
 import Weather from './Weather'
 import Todo from './Todo'
+import Calendar from './Calendar'
+import News from './News'
+import Fact from './Fact'
 
 const Menu = () => {
     const items = [
         { component: <Weather />, label: 'Check the weather', icon: 'sun', height: 'max-h-80' },
         { component: <Todo />, label: 'See your todo list', icon: 'todo', height: 'max-h-80'},
-        // { component: <Clock />, label: 'Open your calendar', icon: 'calendar', height: 'max-h-40'},
-        // { component: <Clock />, label: 'Get a new quote', icon: 'quote', height: 'max-h-32'},
-        // { component: <Clock />, label: 'Listen to some music', icon: 'music', height: 'max-h-20' }
+        { component: <Calendar />, label: 'Open your calendar', icon: 'calendar', height: 'max-h-80'},
+        { component: <News />, label: 'Get daily news', icon: 'news', height: 'max-h-80'},
+        { component: <Fact />, label: 'Learn something new', icon: 'music', height: 'max-h-80' }
     ]
 
     return (
-        <div className='flex flex-col mt-2 justify-center items-center w-1/5'>
+        <div className='flex flex-col md:mt-2 justify-center items-center w-1/5'>
             <span className='m-2 select-none text-center w-full text-xl'>What would you like to do?</span>
             {items.map(item => (<MenuItem key={item.label} item={item}/>))}
         </div>
