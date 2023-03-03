a657a82de79d93231a562a0fb2e3c09eb7639943
Jeremy La <jeremylai511@gmail.com>
2023-03-02 21:53:32 -0500

    Prepped for chrome 3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './styles/App.css';
-import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import CenterDisplay from './components/CenterDisplay';
 import Footer from './components/Footer';
 
 function App() {
-  const [hasBg, setHasBg] = useState(false);
-
-  useEffect(() => {
-    const bgUrl = localStorage.getItem('bgUrl');
-    if (bgUrl) {
-      setHasBg(true);
-    }
-  }, []);
 
   return (
     <div id="App" className="flex flex-col h-screen items-center overflow-hidden text-base"
